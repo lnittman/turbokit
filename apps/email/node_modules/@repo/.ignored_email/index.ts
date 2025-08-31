7a63821e883f4b5a01c502f8cc4012ac331118b8
Luke Nittmann <luke.nittmann@gmail.com>
2025-08-31 14:58:22 -0400

    chore: untrack node_modules/.next/.turbo/dist/build from repository; rely on .gitignore

--- apps/email/node_modules/@repo/.ignored_email/index.ts
+++
@@ -1,8 +0,0 @@
-import { Resend } from 'resend';
-import { keys } from './keys';
-
-export const resend = new Resend(keys().RESEND_TOKEN);
-
-// Export templates
-export { ContactTemplate } from './templates/contact';
-export { InviteEmailTemplate } from './templates/invite';
