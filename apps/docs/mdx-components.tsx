b3f69b28258bb06e60d24c4dac8fac223d03e286
Luke Nittmann <luke.nittmann@gmail.com>
2025-09-05 15:08:43 -0400

    docs(apps/docs): add minimal Accordion MDX fallbacks to satisfy content

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -1,12 +1,19 @@
 import type { MDXComponents } from 'mdx/types';
 import defaultComponents from 'fumadocs-ui/mdx';
-// Using only fumadocs default components to keep build simple
+// Lightweight MDX component fallbacks used by docs content
+function Accordion(props: any) { return <div {...props} /> }
+function AccordionItem(props: any) { return <div {...props} /> }
+function AccordionTrigger(props: any) { return <h4 {...props} /> }
+function AccordionContent(props: any) { return <div {...props} /> }
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
     ...components,
     // Add custom components here
-    
+    Accordion,
+    AccordionItem,
+    AccordionTrigger,
+    AccordionContent,
   };
 }
