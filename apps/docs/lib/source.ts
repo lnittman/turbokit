be471eea1dc8aa405bdce76c745720aba7f32c5c
Luke Nittmann <luke.nittmann@gmail.com>
2025-09-05 14:55:37 -0400

    docs(apps/docs): construct MDX source from generated docs/meta arrays

--- apps/docs/lib/source.ts
+++ apps/docs/lib/source.ts
@@ -1,10 +1,10 @@
-import { docs as map } from '@/.source';
+import { docs as collection } from '@/.source';
 import { loader } from 'fumadocs-core/source';
 import { createMDXSource } from 'fumadocs-mdx';
 
 export const source = loader({
   baseUrl: '/docs',
-  source: createMDXSource(map).toFumadocsSource(),
+  source: createMDXSource(collection.docs as any, collection.meta as any),
 });
 
 export const { getPage, getPages, pageTree, generateParams } = source;
